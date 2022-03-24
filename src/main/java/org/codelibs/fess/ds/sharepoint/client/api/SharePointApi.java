de8dc5f5c922bc290b320d00c5e9595b102805e7
Shinsuke Sugaya <shinsuke@apache.org>
2022-03-24 11:39:39 +0900

    modify url encoding

--- src/main/java/org/codelibs/fess/ds/sharepoint/client/api/SharePointApi.java
+++ src/main/java/org/codelibs/fess/ds/sharepoint/client/api/SharePointApi.java
@@ -22,16 +22,19 @@ import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.codelibs.core.lang.StringUtil;
+import org.codelibs.core.stream.StreamUtil;
 import org.codelibs.fess.crawler.Constants;
 import org.codelibs.fess.ds.sharepoint.client.exception.SharePointClientException;
 import org.codelibs.fess.ds.sharepoint.client.exception.SharePointServerException;
@@ -123,12 +126,12 @@ public abstract class SharePointApi<T extends SharePointApiResponse> {
     }
 
     protected String encodeRelativeUrl(final String url) {
-        String result = url;
-        final String[] array = url.split("/");
-        for (final String value : array) {
-            result = result.replace(value, URLEncoder.encode(value, StandardCharsets.UTF_8));
+        if (url == null) {
+            return null;
         }
-        return result.replace("+", "%20");
+        return StreamUtil.stream(StringUtils.splitPreserveAllTokens(url, '/'))
+                .get(stream -> stream.map(s -> URLEncoder.encode(s, StandardCharsets.UTF_8)).collect(Collectors.joining("/")))
+                .replace("+", "%20");
     }
 
     public static class JsonResponse {
---
+++ src/test/java/org/codelibs/fess/ds/sharepoint/client/api/SharePointApiTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2012-2022 CodeLibs Project and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.codelibs.fess.ds.sharepoint.client.api;
+
+import org.codelibs.fess.util.ComponentUtil;
+import org.dbflute.utflute.lastaflute.LastaFluteTestCase;
+import org.junit.Test;
+
+public class SharePointApiTest extends LastaFluteTestCase {
+    @Override
+    protected String prepareConfigFile() {
+        return "test_app.xml";
+    }
+
+    @Override
+    protected boolean isSuppressTestCaseTransaction() {
+        return true;
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        ComponentUtil.setFessConfig(null);
+        super.tearDown();
+    }
+
+    @Test
+    public void test_encodeRelativeUrl() throws Exception {
+        SharePointApi<SharePointApiResponse> sharePointApi = new SharePointApi<SharePointApiResponse>(null, null, null) {
+            @Override
+            public SharePointApiResponse execute() {
+                return null;
+            }
+        };
+
+        assertNull(sharePointApi.encodeRelativeUrl(null));
+        assertEquals("", sharePointApi.encodeRelativeUrl(""));
+        assertEquals("%20", sharePointApi.encodeRelativeUrl(" "));
+        assertEquals("abc123%21.txt", sharePointApi.encodeRelativeUrl("abc123!.txt"));
+        assertEquals("%E3%83%86%E3%82%B9%E3%83%88.txt", sharePointApi.encodeRelativeUrl("テスト.txt"));
+        assertEquals("%E3%83%86%E3%82%B9%E3%83%88//%E3%83%86%E3%82%B9%E3%83%88", sharePointApi.encodeRelativeUrl("テスト//テスト"));
+        assertEquals("///%E3%83%86%E3%82%B9%E3%83%88.txt///", sharePointApi.encodeRelativeUrl("///テスト.txt///"));
+        assertEquals("/%E3%83%86%E3%82%B9%E3%83%88/%E3%83%86%E3%82%B9%E3%83%88.txt", sharePointApi.encodeRelativeUrl("/テスト/テスト.txt"));
+        assertEquals("/%E3%83%86%E3%82%B9%E3%83%88/%E3%83%86%E3%82%B9%E3%83%88%E3%81%A6%E3%81%99%E3%81%A8.txt",
+                sharePointApi.encodeRelativeUrl("/テスト/テストてすと.txt"));
+    }
+}
