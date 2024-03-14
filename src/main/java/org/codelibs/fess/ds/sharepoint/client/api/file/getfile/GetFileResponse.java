6b7577f44b8b484c0d1b45daed1463f4744765df
Shinsuke Sugaya <shinsuke@apache.org>
2024-03-14 23:25:36 +0900

    Reuse response data in SharePoint client

--- src/main/java/org/codelibs/fess/ds/sharepoint/client/api/file/getfile/GetFileResponse.java
+++ src/main/java/org/codelibs/fess/ds/sharepoint/client/api/file/getfile/GetFileResponse.java
@@ -15,25 +15,65 @@
  */
 package org.codelibs.fess.ds.sharepoint.client.api.file.getfile;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.io.output.DeferredFileOutputStream;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.codelibs.core.io.CopyUtil;
 import org.codelibs.fess.ds.sharepoint.client.api.SharePointApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GetFileResponse implements SharePointApiResponse {
+    private static final Logger logger = LoggerFactory.getLogger(GetFileResponse.class);
+
     private final CloseableHttpResponse httpResponse;
+
+    private final int cacheFileSize = 1_000_000;
+
+    private byte[] responseData;
+
+    private File responseFile;
 
     public GetFileResponse(final CloseableHttpResponse httpResponse) {
         this.httpResponse = httpResponse;
     }
 
     public InputStream getFileContent() throws IOException {
-        return httpResponse.getEntity().getContent();
+        if (responseData == null && responseFile == null) {
+            HttpEntity entity = null;
+            try (DeferredFileOutputStream out =
+                    DeferredFileOutputStream.builder().setThreshold(cacheFileSize).setPrefix("fess-extractor-").setSuffix(".out").get()) {
+                entity = httpResponse.getEntity();
+                CopyUtil.copy(entity.getContent(), out);
+                out.flush();
+
+                if (out.isInMemory()) {
+                    responseData = out.getData();
+                } else {
+                    responseFile = out.getFile();
+                }
+            } finally {
+                EntityUtils.consumeQuietly(entity);
+            }
+        }
+        if (responseData != null) {
+            return new ByteArrayInputStream(responseData);
+        }
+        return new FileInputStream(responseFile);
     }
 
     @Override
     public void close() throws IOException {
+        if (responseFile != null && !responseFile.delete()) {
+            logger.warn("Failed to delete {}.", responseFile.getAbsolutePath());
+        }
         httpResponse.close();
     }
 }
